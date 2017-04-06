a19a7149f018eebab07e423e89075cd7c999642b
Oleksii Polischuk <polischuk.kiev@gmail.com>
2017-04-06 23:37:42 +0300

    moved to separate project

---
+++ WaypointCalculator/tests/TestPoint.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import calculator.App;
+import geometry.Point;
+
+
+public class TestPoint {
+	
+   @Test
+   public void distanceTo() {
+	   
+   }
+}
