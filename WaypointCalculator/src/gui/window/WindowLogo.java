c000b54f93106c6e39c172155a579218d1975623
Oleksii Polischuk <polischuk.kiev@gmail.com>
2017-04-21 20:54:30 +0300

    about to release 1.2

--- WaypointCalculator/src/gui/window/WindowLogo.java
+++ WaypointCalculator/src/gui/window/WindowLogo.java
@@ -71,12 +71,12 @@ public class WindowLogo extends JWindow implements ActionListener {
 		logger.info("Developed by Oleksii Polishchuk");
 		Logger.unsubscribe(label);
 		
-//		try {
-//			Thread.sleep(3000);
-//		} catch (InterruptedException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		this.setVisible(false);
 		WindowMain mw= new WindowMain();
